84cf3f6fa614a17f9fcb98e525aee88aa39b1ca5
Josh Hootman <hootmanj@objectcomputing.com>
2023-12-15 16:00:21 -0600

    add basica timestamp component

---
+++ src/lib/components/CreatedAt.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Clock from './Svg/outline/Clock.svelte';
+
+	export let timestamp: string;
+</script>
+
+<div class="flex item-center gap-x-1">
+	<Clock />
+
+	<time class="text-sm">
+		{new Date(timestamp).toLocaleString('en-US', { timeZone: 'UTC' })}
+	</time>
+</div>
