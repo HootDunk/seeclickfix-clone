187febc639cdc5e6180a2867c55712607a326609
Josh Hootman <hootmanj@objectcomputing.com>
2023-12-15 16:00:51 -0600

    first pass on service reqeust cards

--- src/lib/components/IssuesList/IssuesList.svelte
+++ src/lib/components/IssuesList/IssuesList.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import type { IssuesResponse } from '$lib/services/SeeClickFix/SeeClickFixService';
-	import { Card } from 'stwui';
+	import { Button, Card, Chip, Icon } from 'stwui';
+	import IssueStatusIndicator from '../IssueStatus/IssueStatusIndicator.svelte';
+	import Clock, { clockSvg } from '../Svg/outline/Clock.svelte';
+	import CreatedAt from '../CreatedAt.svelte';
 
 	export let issuesResponse: IssuesResponse;
+	// todo use grid for all items in content div
 </script>
 
-<ul>
+<ul class="mx-4">
 	{#each issuesResponse.issues as item (item.id)}
 		<li class="mb-4">
 			<Card>
-				<!-- <Card.Header slot="header">{item.summary}</Card.Header> -->
-
 				<div slot="content">
-					{item.summary}
 					{#if item.media.image_full}
 						<img
 							src={item.media.image_full}
@@ -20,9 +21,29 @@
 							class="object-cover object-center w-full aspect-1 h-[300px]"
 						/>
 					{/if}
-					testing below img
+					<div class="issues-list-button-override space-y-1 p-4">
+						<Button class="p-0" type="link" href="https://google.com">
+							<h2>
+								{item.summary}
+							</h2>
+						</Button>
+						<div class="truncate">{item.address}</div>
+						<div class="flex gap-4">
+							<IssueStatusIndicator status={item.status} />
+							<CreatedAt timestamp={item.created_at} />
+						</div>
+						<div class="truncate">
+							{item.description}
+						</div>
+					</div>
 				</div>
 			</Card>
 		</li>
 	{/each}
 </ul>
+
+<style>
+	.issues-list-button-override :global(.stwui-button-hover) {
+		opacity: 0;
+	}
+</style>
