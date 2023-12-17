d125dc2ad1860614a47434c7873a4afff97bb5a4
Josh Hootman <hootmanj@objectcomputing.com>
2023-12-17 12:46:42 -0600

    update to show issue on map

--- src/lib/components/GoogleMap.svelte
+++ src/lib/components/GoogleMap.svelte
@@ -1,11 +1,53 @@
+<script lang="ts" context="module">
+	import openPng from '$lib/assets/open.png';
+	import closedPng from '$lib/assets/closed.png';
+	import acknowledgedPng from '$lib/assets/acknowledged.png';
+	import archivedPng from '$lib/assets/archivedPng.png';
+
+	const statusToPngMap = {
+		Open: openPng,
+		Closed: closedPng,
+		Acknowledged: acknowledgedPng,
+		Archived: archivedPng
+	};
+
+	// likely will want to create a map of Map<IssueId, Marker>  so that we can update individual markers on click
+	// can the 'selected' marker and have it hidden, on click hide the issue marker and update selected marker position
+	function createIssueMarker(issue: IssuesResponse['issues'][0], map: google.maps.Map) {
+		return new google.maps.Marker({
+			position: {
+				lat: issue.lat,
+				lng: issue.lng
+			},
+			map,
+			title: `Issue: ${issue.summary}`,
+			clickable: true,
+			icon: {
+				url: statusToPngMap[issue.status],
+				scaledSize: new google.maps.Size(22, 22)
+			}
+		});
+	}
+
+	function createIssueMarkers(issues: Array<IssuesResponse['issues'][0]>, map?: google.maps.Map) {
+		if (!map) return;
+		// todo use reduce function instead of map and create issueIdIssueMarkerMap
+		return issues.map((issue) => createIssueMarker(issue, map));
+	}
+</script>
+
 <script lang="ts">
+	import type { AsyncResult, IssuesResponse } from '$lib/services/SeeClickFix/SeeClickFixService';
 	import { Loader } from '@googlemaps/js-api-loader';
 	import { onMount } from 'svelte';
 
+	// whenever this value changes, rerender the issues on the map
+	export let issuesResponse: AsyncResult<IssuesResponse>;
+
 	let mapElement: HTMLDivElement;
 	let map: google.maps.Map;
+
 	onMount(() => {
-		// todo may not need to load onMount (but probably do)
 		const loader = new Loader({
 			apiKey: 'AIzaSyDo-j3odI6X6jwwpwA1JH3xzxc-PGtHo74',
 			version: 'weekly'
@@ -36,10 +78,13 @@
 				zoomControlOptions: {
 					position: google.maps.ControlPosition.TOP_LEFT
 				},
-				fullscreenControl: false
+				fullscreenControl: false,
+				draggableCursor: 'default'
 			});
 		});
 	});
+
+	$: if (issuesResponse.type === 'success') createIssueMarkers(issuesResponse.value.issues, map);
 </script>
 
 <div bind:this={mapElement} />
--- src/routes/[scf_token]/issues/map/+layout.svelte
+++ src/routes/[scf_token]/issues/map/+layout.svelte
@@ -2,17 +2,50 @@
 	import { onMount } from 'svelte';
 	import LandingPageLayout from '../../../../lib/pages/LandingPage/LangingPageLayout.svelte';
 	import GoogleMap from '$lib/components/GoogleMap.svelte';
+	import {
+		MockSeeClickFixService,
+		asAsyncSuccess,
+		type SeeClickFixService,
+		ASYNC_IN_PROGRESS,
+		type AsyncResult,
+		type IssuesResponse
+	} from '$lib/services/SeeClickFix/SeeClickFixService';
 	onMount(() => console.log('layout mount'));
 
 	// when I click on an issue, the issue details are shown on the left side.
 	// when I click back, then the issue details list is shown
 
-	// layout component will need to have the map and then forward the slot of the side bar
-
 	// can access the lat and lng from the route params and use them to load data from within the different +page.svelte components
+
+	// todo mobile view map overlay (click single issue on map => shows card over map.  if issue is selected in desktop and mobile breakpoint is hit, then it shows card overlay)
+
+	// need to load the data here so that we can create a context/store for access from both the side-bar and the main-content
+
+	const seeClickFixService: SeeClickFixService = new MockSeeClickFixService();
+	let issuesResponse: AsyncResult<IssuesResponse> = ASYNC_IN_PROGRESS;
+	onMount(loadIssues);
+
+	async function loadIssues(page: number = 1) {
+		const res = await seeClickFixService.getIssues({
+			area: {
+				min_lat: 36.03282181999068,
+				max_lat: 6.093566455321536,
+				min_lng: -94.17250121704095,
+				max_lng: -94.14331878295897
+			},
+			statuses: ['Acknowledged', 'Closed', 'Open'],
+			page
+		});
+		issuesResponse = asAsyncSuccess(res);
+	}
+
+	// map can dispatch
+
+	// entire app can useSeeClickFixService
+	// inside if here we will likely create an issuesContext.  for now lets just pass the data into the map directly and work on the issues display.  that may have some implications on how we design our context/store
 </script>
 
 <LandingPageLayout>
 	<slot slot="side-bar" />
-	<GoogleMap slot="main-content" />
+	<GoogleMap {issuesResponse} slot="main-content" />
 </LandingPageLayout>
